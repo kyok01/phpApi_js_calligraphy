df20424327b7449667e9ccd1cb993fdf634ef2c4
kyok01 <kazutakayokoils@gmail.com>
2022-06-18 17:26:54 +0900

    multiple pages are deployed

---
+++ data/folder2/main.js
@@ -0,0 +1,4 @@
+let str="";
+const arr=["a", "b","c"];
+arr.forEach((ele)=> str=str+ele);
+document.querySelector("p").innerHTML=str;
--- fileWrite.php
+++ fileWrite.php
@@ -6,19 +6,20 @@ if($_SERVER["HTTP_HOST"] == 'localhost'){
     header("Access-Control-Allow-Headers: X-Requested-With");
 }
 
+$artId = $_POST["artId"];
 $html = $_POST["html"];
 $js = $_POST["js"];
 $css = $_POST["css"];
 
-$file = fopen("./data/test.html", "w" ); //ファイルOPEN
+$file = fopen("./data/folder{$artId}/sample.html", "w" ); //ファイルOPEN
 fwrite( $file, $html); //書込みです
 fclose( $file ); //ファイル閉じる
 
-$file = fopen("./data/main.js", "w" ); //ファイルOPEN
+$file = fopen("./data/folder{$artId}/main.js", "w" ); //ファイルOPEN
 fwrite( $file, $js); //書込みです
 fclose( $file ); //ファイル閉じる
 
-$file = fopen("./data/style.css", "w" ); //ファイルOPEN
+$file = fopen("./data/folder{$artId}/style.css", "w" ); //ファイルOPEN
 fwrite( $file, $css); //書込みです
 fclose( $file ); //ファイル閉じる
 
