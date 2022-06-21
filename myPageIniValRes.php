38efa8132c22d435c3b7f08801a484543960e94d
kyok01 <kazutakayokoils@gmail.com>
2022-06-21 13:23:55 +0900

    temporary save

---
+++ myPageIniValRes.php
@@ -0,0 +1,29 @@
+<?php
+// CORS permission
+if($_SERVER["HTTP_HOST"] == 'localhost'){
+    header('Access-Control-Allow-Origin: http://localhost:3000');
+    header('Access-Control-Allow-Methods: GET, POST');
+    header("Access-Control-Allow-Headers: X-Requested-With");
+}
+
+$art_id = $_POST["art_id"];
+$lid = $_POST["lid"];
+
+include("./library/funcs.php");  //funcs.phpを読み込む（関数群）
+$pdo = db_conn();      //DB接続関数
+
+//２．データ登録SQL作成
+$stmt   = $pdo->prepare("SELECT * FROM mypage_art WHERE art_id=:art_id AND lid=:lid"); //SQLをセット
+$stmt->bindValue(':art_id',  $art_id,   PDO::PARAM_INT);  //Integer（数値の場合 PDO::PARAM_INT)
+$stmt->bindValue(':lid',  $lid,   PDO::PARAM_STR);  //Integer（数値の場合 PDO::PARAM_INT)
+$status = $stmt->execute(); //SQLを実行→エラーの場合falseを$statusに代入
+
+//３．データ表示
+if($status==false) {
+  //SQLエラーの場合
+  sql_error($stmt);
+}else{
+  //SQL成功の場合
+  $r = $stmt->fetchAll();
+  echo json_encode($r);
+}
